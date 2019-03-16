4eaa0bd3b9df4f877759ccf17810e50287743fdd
ikostan <igorkostan@gmail.com>
2019-03-16 16:20:02 -0700

    code refactoring
    Now solution is more generic

--- Problem_1/problem_1.py
+++ Problem_1/problem_1.py
@@ -4,17 +4,24 @@
 import time
 
 
-def get_multiplies_of_3_and_5(num):
+def get_multiplies_of_3_and_5(max_num, nums):
     start = time.time()
     result = 0
-    i = 3
+    i = 0
 
-    while i < num:
-        if i % 3 == 0 or i % 5 == 0:
+    while i < max_num:
+        if div_by_nums(i, nums):
             result += i
         i += 1
 
     elapsed = time.time() - start
     print("result {0} returned in {1} seconds".format(result, elapsed))
     return result
+
+
+def div_by_nums(i, nums):
+    for n in nums:
+        if i % n == 0:
+            return True
+    return False
 
--- Problem_1/unit_tests.py
+++ Problem_1/unit_tests.py
@@ -8,13 +8,15 @@ from problem_1 import get_multiplies_of_3_and_5
 class MyTestCase(unittest.TestCase):
     def test_basic(self):
         result = 23
-        number = 10
-        self.assertEqual(result, get_multiplies_of_3_and_5(number))
+        max_num = 10
+        nums = [3, 5]
+        self.assertEqual(result, get_multiplies_of_3_and_5(max_num, nums))
 
     def test_1000(self):
         result = 233168
-        number = 1000
-        self.assertEqual(result, get_multiplies_of_3_and_5(number))
+        max_num = 1000
+        nums = [3, 5]
+        self.assertEqual(result, get_multiplies_of_3_and_5(max_num, nums))
 
 
 if __name__ == '__main__':
