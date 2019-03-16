8e66e0e967a97995a7edef9acda07c75e9404855
ikostan <igorkostan@gmail.com>
2019-03-16 16:25:00 -0700

    New unit test
    New unit test added

--- Problem_1/unit_tests.py
+++ Problem_1/unit_tests.py
@@ -6,9 +6,15 @@ from problem_1 import get_multiplies_of_3_and_5
 
 
 class MyTestCase(unittest.TestCase):
-    def test_basic(self):
+    def test_10(self):
         result = 23
         max_num = 10
+        nums = [3, 5]
+        self.assertEqual(result, get_multiplies_of_3_and_5(max_num, nums))
+
+    def test_100(self):
+        result = 2318
+        max_num = 100
         nums = [3, 5]
         self.assertEqual(result, get_multiplies_of_3_and_5(max_num, nums))
 
