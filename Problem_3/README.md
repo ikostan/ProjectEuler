8f8e7f2a69a4ac82f893e5c64a78687011e29e04
ikostan <igorkostan@gmail.com>
2019-03-16 17:56:16 -0700

    First logic + readme update

--- Problem_3/README.md
+++ Problem_3/README.md
@@ -6,3 +6,6 @@ What is the largest prime factor of the number 600851475143 ?
 
 ### Video tutorial: 
 https://www.youtube.com/watch?v=QNMrJ3An8eE&list=PLNEKkZrJ3NV-wNjCMxPS86sgqOgwBZmPu&index=28
+
+### More about prime factorization: 
+https://www.youtube.com/watch?v=ZKKDTfHcsG0
--- Problem_3/problem_3.py
+++ Problem_3/problem_3.py
@@ -5,12 +5,22 @@ import time
 
 def get_largest_prime(number):
     start_time = time.time()
-    result = 0
+    result = max(find_prime_numbers(number))
     print("result {0} returned in {1} seconds".format(result, time.time() - start_time))
     return result
 
 
 def find_prime_numbers(number):
-    numbers = list()
-    return max(numbers)
+    prime_numbers = list()
+    for i in range(2, number):
+        if is_prime(i):
+            prime_numbers.append(i)
+    return prime_numbers
+
+
+def is_prime(i):
+    for n in range(2, i):
+        if i % n == 0:
+            return False
+    return True
 
