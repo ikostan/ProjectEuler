83f93995d417e36cad8d58a20b2f7e4557fd1663
ikostan <igorkostan@gmail.com>
2019-03-17 18:08:41 -0700

    is_palindrome implementation

--- Problem_4/problem_4.py
+++ Problem_4/problem_4.py
@@ -2,3 +2,16 @@
 
 
 import time
+
+
+def is_palindrome(number):
+    word = str(number)
+
+    if len(word) % 2 != 0:
+        return False
+
+    if word == word[::-1]:
+        return True
+
+    return False
+
--- Problem_4/unit_tests.py
+++ Problem_4/unit_tests.py
@@ -1,9 +1,12 @@
 import unittest
+from problem_4 import is_palindrome
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_is_palindrome(self):
+        number = 9009
+        self.assertEqual(True, is_palindrome(number))
 
 
 if __name__ == '__main__':
