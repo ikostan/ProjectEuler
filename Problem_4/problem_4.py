c506736457d3b8050fd041892aa6ad843ca473e9
ikostan <igorkostan@gmail.com>
2019-03-17 18:12:34 -0700

    is_palendrome refactoring
    function refactoring + new unit tests

--- Problem_4/problem_4.py
+++ Problem_4/problem_4.py
@@ -7,9 +7,6 @@ import time
 def is_palindrome(number):
     word = str(number)
 
-    if len(word) % 2 != 0:
-        return False
-
     if word == word[::-1]:
         return True
 
--- Problem_4/unit_tests.py
+++ Problem_4/unit_tests.py
@@ -4,9 +4,21 @@ from problem_4 import is_palindrome
 
 class MyTestCase(unittest.TestCase):
 
-    def test_is_palindrome(self):
+    def test_is_palindrome_basic(self):
         number = 9009
         self.assertEqual(True, is_palindrome(number))
+
+    def test_is_palindrome_negative(self):
+        number = 9007
+        self.assertEqual(False, is_palindrome(number))
+
+    def test_is_palindrome_5_digits(self):
+        number = 90709
+        self.assertEqual(True, is_palindrome(number))
+
+    def test_is_palindrome_5_digits_negative(self):
+        number = 90705
+        self.assertEqual(False, is_palindrome(number))
 
 
 if __name__ == '__main__':
