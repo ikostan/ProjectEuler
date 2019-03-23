407558feee27cbef8fd0b1a34bfea066a0cabdb7
ikostan <igorkostan@gmail.com>
2019-03-22 21:04:29 -0700

    Basic unitests ready

--- Problem_6/problem_6.py
+++ Problem_6/problem_6.py
@@ -4,6 +4,23 @@
 import time
 
 
+def square_diff(start, end):
+    start_time = time.time()
+    result = square_of_sums(start, end) - sum_squares_of_numbers(start, end)
+    end_time = time.time() - start_time
+    print_log(end_time, result)
+    return result
+
+def sum_squares_of_numbers(start, end):
+    result = 0
+    return result
+
+
+def square_of_sums(start, end):
+    result = 0
+    return result
+
+
 def print_log(end_time, result):
     if end_time < 60:
         print("The answer {0} returned in {1} seconds".format(result, end_time))
--- Problem_6/unit_tests.py
+++ Problem_6/unit_tests.py
@@ -1,9 +1,28 @@
 import unittest
+from problem_6 import sum_squares_of_numbers
+from problem_6 import square_of_sums
+from problem_6 import  square_diff
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_sum_of_squares(self):
+        expected = 385
+        start = 1
+        end = 10
+        self.assertEqual(expected, sum_squares_of_numbers(start, end))
+
+    def test_square_of_sums(self):
+        expected = 3025
+        start = 1
+        end = 10
+        self.assertEqual(expected, square_of_sums(start, end))
+
+    def test_basic(self):
+        start = 1
+        end = 10
+        expected = 2640
+        self.assertEqual(expected, square_diff(start, end))
 
 
 if __name__ == '__main__':
