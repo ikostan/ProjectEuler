b4fc99815c2b009bf475e3721ac34c8eaa73484c
ikostan <igorkostan@gmail.com>
2019-03-23 15:53:11 -0700

    Solution ready
    Solution + PDF

--- Problem_9/problem_9.py
+++ Problem_9/problem_9.py
@@ -2,14 +2,33 @@
 
 
 import time
+import math
 
 
-def find_triplet(number:int):
+def find_triplet(num: int):
     start_time = time.time()
+    a, b = 1, 1
     result = 0
-    end_time = time.time() - start_time
-    print_log(end_time, result)
-    return result
+
+    while a < (num - 2):
+        b = a + 1
+        while b < (num - 3):
+            c = num - (a + b)
+
+            if c < b:
+                break
+
+            if a < b < c:
+                if math.pow(a, 2) + math.pow(b, 2) == math.pow(c, 2):
+                    print("a:{0}, b:{1}, c:{2}".format(a, b, c))
+                    result = a*b*c
+                    end_time = time.time() - start_time
+                    print_log(end_time, result)
+                    return result
+            b += 1
+        a += 1
+
+
 
 
 def print_log(end_time, result):
@@ -19,3 +38,4 @@ def print_log(end_time, result):
         minutes = int(end_time / 60)
         seconds = end_time - (minutes * 60)
         print("The answer {0} returned in {1} minutes and {2} seconds".format(result, minutes, seconds))
+
--- Problem_9/unit_tests.py
+++ Problem_9/unit_tests.py
@@ -1,9 +1,20 @@
 import unittest
+from problem_9 import  find_triplet
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_basic(self):
+        a = 3
+        b = 4
+        c = 5
+        num = a + b + c
+        expected = a * b * c
+        self.assertEqual(expected, find_triplet(num))
+
+    def test_1000(self):
+        num = 1000
+        expected = 31875000
+        self.assertEqual(expected, find_triplet(num))
 
 
 if __name__ == '__main__':
