2a7b13c2ce3a9e506037c132861a7a96fff4e795
ikostan <igorkostan@gmail.com>
2019-03-23 13:46:24 -0700

    Problem #7 solved

--- Problem_7/problem_7.py
+++ Problem_7/problem_7.py
@@ -5,6 +5,39 @@ import time
 import math
 
 
+def prime_generator(limit: int):
+    start_time = time.time()
+    primes = list()
+    n = 2
+
+    while len(primes) < limit:
+        if is_prime(n):
+            primes.append(n)
+        n += 1
+
+    end_time = time.time() - start_time
+    print_log(end_time, primes[limit - 1])
+    return primes[limit - 1]
+
+
+def is_prime(n: int):
+
+    if n == 1:
+        return False
+
+    if n == 2:
+        return True
+
+    if n > 2 and n % 2 == 0:
+        return False
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
+
+
 def print_log(end_time, result):
     if end_time < 60:
         print("The answer {0} returned in {1} seconds".format(result, end_time))
--- Problem_7/uit_tests.py
+++ Problem_7/uit_tests.py
@@ -1,9 +1,18 @@
 import unittest
+from problem_7 import prime_generator
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_basic(self):
+        limit = 6
+        expected = 13
+        self.assertEqual(expected, prime_generator(limit))
+
+    def test_10001(self):
+        limit = 10001
+        expected = 104743
+        self.assertEqual(expected, prime_generator(limit))
 
 
 if __name__ == '__main__':
