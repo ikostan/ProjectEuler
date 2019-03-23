6db10e9c63f5c445d3a03303b226c96c99c255b9
ikostan <igorkostan@gmail.com>
2019-03-22 20:54:13 -0700

    Start problem #6

---
+++ Problem_6/problem_6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+import time
+
+
+def print_log(end_time, result):
+    if end_time < 60:
+        print("The answer {0} returned in {1} seconds".format(result, end_time))
+    else:
+        minutes = int(end_time / 60)
+        seconds = end_time - (minutes * 60)
+        print("The answer {0} returned in {1} minutes and {2} seconds".format(result, minutes, seconds))
