679b8964d2bf3aca40ed3ed15c5d5371e049ac34
ikostan <igorkostan@gmail.com>
2019-03-24 16:56:46 -0700

    Solution is ready
    Unit test pass

--- Problem_13/problem_13.py
+++ Problem_13/problem_13.py
@@ -4,6 +4,15 @@
 import time
 
 
+def get_first_ten_digit(numbers: list, limit: int):
+    start_time = time.time()
+    string_number = str(sum(numbers))
+    result = int(string_number[:limit])
+    end_time = time.time() - start_time
+    print_time_log(end_time, result)
+    return result
+
+
 def print_time_log(end_time: time, result):
     if end_time < 60:
         print("The answer {0} returned in {1} seconds".format(
