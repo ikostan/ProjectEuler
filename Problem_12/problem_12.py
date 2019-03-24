a8272caee9c832790a114ac05875bf47cee173ef
ikostan <igorkostan@gmail.com>
2019-03-24 14:51:27 -0700

    Basic logic is ready

--- Problem_12/problem_12.py
+++ Problem_12/problem_12.py
@@ -4,11 +4,24 @@
 import time
 
 
-def print_log(end_time, result):
+def triangle_number_generator(limit: int):
+    triangle = {'counter': 0, 'total': 0, 'number': 1}
+    while triangle['number'] <= limit:
+        triangle['total'] += triangle['number']
+        triangle['counter'] += 1
+        triangle['number'] += 1
+    # Debug only:
+    print("counter:{0}, number:{1}".format(triangle['counter'], triangle['total']))
+    return triangle
+
+
+def print_time_log(end_time: time, result):
     if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(result, end_time))
+        print("The answer {0} returned/"
+              " in {1} seconds".format(result, end_time))
     else:
         minutes = int(end_time / 60)
         seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(result, minutes, seconds))
+        print("The answer {0} returned/"
+              " in {1} minutes and {2} seconds".format(result, minutes, seconds))
 
--- Problem_12/unit_tests.py
+++ Problem_12/unit_tests.py
@@ -1,9 +1,12 @@
 import unittest
+from problem_12 import triangle_number_generator
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_triangle_number_generator(self):
+        counter = 7
+        expected = 28
+        self.assertEqual(expected, triangle_number_generator(counter)['total'])
 
 
 if __name__ == '__main__':
