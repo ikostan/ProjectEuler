ac6c55ceca3a90ae3f0e44ef3fb57274028d4b08
ikostan <igorkostan@gmail.com>
2019-03-24 18:37:38 -0700

    Solution is ready
    Solution + unit tests

--- Problem_14/problem_14.py
+++ Problem_14/problem_14.py
@@ -2,9 +2,41 @@
 
 
 import time
+import math
 
 
-def collatz_sequence(starting_number: int, is_first_run=True):
+def get_max_counter(starting_number: int):
+    start_time = time.time()
+    result = {'number': 1, 'counter': 1}
+
+    while starting_number > 0:
+        temp = get_collatz_counter(starting_number)
+        if result['counter'] < temp:
+            result['counter'] = temp
+            result['number'] = starting_number
+        starting_number -= 1
+
+    end_time = time.time() - start_time
+    print("number: {0} has {1} members chain".format(result['number'], result['counter']))
+    print_time_log(end_time, result['number'])
+    return result
+
+
+def get_collatz_counter(starting_number: int):
+    counter = 1
+    n = starting_number
+
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = (n * 3) + 1
+        counter += 1
+
+    return counter
+
+
+def collatz_sequence(starting_number: int, is_first_run=True, counter=1):
 
     if is_first_run:
         # Printing with no newlines in Python 3
@@ -12,7 +44,8 @@ def collatz_sequence(starting_number: int, is_first_run=True):
 
     # Terminate execution
     if starting_number == 1:
-        print(str(1), end='')
+        print(str(1))
+        print("Counter: {0}".format(counter))
         return
 
     # Collatz sequence
@@ -25,7 +58,8 @@ def collatz_sequence(starting_number: int, is_first_run=True):
         # Printing with no newlines in Python 3
         print(str(starting_number) + " -> ", end='')
 
-    collatz_sequence(starting_number, False)
+    counter += 1
+    collatz_sequence(starting_number, False, counter)
 
 
 # This function is used for logging processing time only
--- Problem_14/unit_tests.py
+++ Problem_14/unit_tests.py
@@ -1,12 +1,40 @@
 import unittest
 from problem_14 import collatz_sequence
+from problem_14 import get_max_counter
+from problem_14 import get_collatz_counter
 
 
 class MyTestCase(unittest.TestCase):
-    def test_basic(self):
+
+    def test_get_collatz_counter_13(self):
+        starting_number = 13
+        expected = 10
+        self.assertEqual(expected, get_collatz_counter(starting_number))
+
+    def test_get_collatz_counter_9(self):
+        starting_number = 9
+        expected = 20
+        self.assertEqual(expected, get_collatz_counter(starting_number))
+
+    def test_collatz_sequence_13(self):
         starting_number = 13
         collatz_sequence(starting_number)
         self.assertEqual(True, True)
+
+    def test_collatz_sequence_9(self):
+        starting_number = 9
+        collatz_sequence(starting_number)
+        self.assertEqual(True, True)
+
+    def test_basic(self):
+        starting_number = 13
+        expected = 9
+        self.assertEqual(expected, get_max_counter(starting_number)['number'])
+
+    def test_large(self):
+        starting_number = 999999
+        expected = 837799
+        self.assertEqual(expected, get_max_counter(starting_number)['number'])
 
 
 if __name__ == '__main__':
