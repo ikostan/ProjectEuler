ba511604cb1b974ee669447aab766e361f4fad51
ikostan <igorkostan@gmail.com>
2019-03-24 17:51:49 -0700

    Basic logic is ready

--- Problem_14/problem_14.py
+++ Problem_14/problem_14.py
@@ -4,6 +4,30 @@
 import time
 
 
+def collatz_sequence(starting_number: int, is_first_run=True):
+
+    if is_first_run:
+        # Printing with no newlines in Python 3
+        print(str(starting_number) + " -> ", end='')
+
+    # Terminate execution
+    if starting_number == 1:
+        print(str(1), end='')
+        return
+
+    # Collatz sequence
+    if starting_number % 2 == 0:
+        starting_number = starting_number // 2
+    else:
+        starting_number = (starting_number * 3) + 1
+
+    if starting_number != 1:
+        # Printing with no newlines in Python 3
+        print(str(starting_number) + " -> ", end='')
+
+    collatz_sequence(starting_number, False)
+
+
 # This function is used for logging processing time only
 # Shows how long it took in order to get the answer
 def print_time_log(end_time: time, result):
@@ -15,3 +39,4 @@ def print_time_log(end_time: time, result):
         seconds = end_time - (minutes * 60)
         print("The answer {0} returned in {1} minutes and {2} seconds".format(
             result, minutes, seconds))
+
--- Problem_14/unit_tests.py
+++ Problem_14/unit_tests.py
@@ -1,9 +1,12 @@
 import unittest
+from problem_14 import collatz_sequence
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_basic(self):
+        starting_number = 13
+        collatz_sequence(starting_number)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
