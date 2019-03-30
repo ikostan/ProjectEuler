00bb3a8c21c6776b4be44fcfb47f7a91ed346227
ikostan <igorkostan@gmail.com>
2019-03-30 12:58:34 -0700

    Unit tests problem 7 refactoring

--- Problem_7/unit_tests_problem_7.py
+++ Problem_7/unit_tests_problem_7.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python
+
+
 import unittest
+import os
 from Problem_7.problem_7 import prime_generator
 
 
 class MyTestCase(unittest.TestCase):
+
+    print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
     def test_basic(self):
         limit = 6
