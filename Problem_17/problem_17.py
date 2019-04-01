6a5df5ca06a59ba1534886bf07ca058d993462ad
ikostan <igorkostan@gmail.com>
2019-03-31 18:56:31 -0700

    Basic test case pass

--- Problem_17/problem_17.py
+++ Problem_17/problem_17.py
@@ -4,6 +4,31 @@
 import time
 
 
+def number_to_words(numbers: list):
+
+    start_time = time.time()
+
+    nums_0_19 = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six',
+                 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen',
+                 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+
+    nums_20_90 = ['Twenty', 'Thirty', 'Forty', 'Fifty',
+                  'Sixty', 'Seventy', 'Eighty', 'Ninety']
+
+    nums_dict = {100: 'hundred', 1000: 'thousand',
+                 1000000: 'million', 1000000000: 'billion'}
+
+    result = ''
+
+    for n in numbers:
+        if n < 20:
+            result += nums_0_19[n]
+
+    end_time = time.time() - start_time
+    print_time_log(end_time, result)
+    return result
+
+
 # This function is used for logging processing time only
 # Shows how long it took in order to get the answer
 def print_time_log(end_time: time, result):
--- Problem_17/unit_tests_problem_17.py
+++ Problem_17/unit_tests_problem_17.py
@@ -3,14 +3,26 @@
 
 import unittest
 import os
+from Problem_17.problem_17 import number_to_words
 
 
 class MyTestCase(unittest.TestCase):
 
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
-    def test_sample(self):
-        self.assertEqual(True, False)
+    def test_basic(self):
+        start = 1
+        end = 5
+        numbers = list(range(start, end + 1))
+        expected = 19
+        self.assertEqual(expected, len(number_to_words(numbers)))
+
+    def test_big(self):
+        start = 1
+        end = 1000
+        numbers = list(range(start, end + 1))
+        expected = 19
+        self.assertEqual(expected, len(number_to_words(numbers)))
 
 
 if __name__ == '__main__':
