98c770b6c3cdcf6b0370ccba275b216a62f1c82c
ikostan <igorkostan@gmail.com>
2019-04-07 20:05:32 -0700

    unit test test_big fixes

--- Problem_18/unit_tests_problem_18.py
+++ Problem_18/unit_tests_problem_18.py
@@ -25,17 +25,6 @@ class MyTestCase(unittest.TestCase):
         expected = sum([5, 9, 6, 7])
         self.assertEqual(expected, find_maximum_total(triangle))
 
-    def test_medium(self):
-        triangle = [[75],
-                    [95, 64],
-                    [17, 47, 82],
-                    [18, 35, 87, 10],
-                    [20, 4, 82, 47, 65],
-                    [19, 1, 23, 75, 3, 34],
-                    [88, 2, 77, 73, 7, 63, 67]]
-        expected = sum([75, 95, 47, 87, 82, 75, 73])
-        self.assertEqual(expected, find_maximum_total(triangle))
-
     def test_big(self):
         triangle = [[75],
                     [95, 64],
@@ -51,8 +40,9 @@ class MyTestCase(unittest.TestCase):
                     [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
                     [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
                     [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-                    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],]
-        expected = 1074
+                    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+
+        expected = sum([75, 64, 82, 87, 82, 75, 73, 28, 83, 32, 91, 78, 58, 73, 93])
         self.assertEqual(expected, find_maximum_total(triangle))
 
 
