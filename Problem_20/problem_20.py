0892fa4dbff16f11da0433c19c684b29b9d37bc9
ikostan <igorkostan@gmail.com>
2019-06-20 16:15:35 -0700

    Problem #20 solved

--- Problem_20/problem_20.py
+++ Problem_20/problem_20.py
@@ -4,6 +4,38 @@
 import time
 
 
+def calc_sum_of_digits(n: int):
+    '''
+    Calculates the sum of the digits in the number
+    :param n:
+    :return:
+    '''
+
+    start_time = time.time()
+    factorial = calc_factorial(n)
+    sum_of_digits = eval(' + '.join([i for i in str(factorial)]))
+
+    end_time = time.time() - start_time
+    print_time_log(end_time, sum_of_digits)
+
+    return sum_of_digits
+
+
+def calc_factorial(n: int):
+    '''
+    Calculate factorial for the number: n!
+    :param n:
+    :return:
+    '''
+
+    result = 1
+
+    for num in range(1, n + 1):
+        result *= num
+
+    return result
+
+
 # This function is used for logging processing time only
 # Shows how long it took in order to get the answer
 def print_time_log(end_time: time, result):
