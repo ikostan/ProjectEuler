223c70b4a7696b64d30ef8657dd3a58d2a8ec802
ikostan <igorkostan@gmail.com>
2019-06-19 17:00:01 -0700

    Problem 67 -> solved

--- Problem_67/problem_67.py
+++ Problem_67/problem_67.py
@@ -2,6 +2,57 @@
 
 
 import time
+import os
+
+
+def find_maximum_total(triangle: list):
+    start_time = time.time()
+    new_triangle = triangle
+
+    for n in range(0, len(new_triangle) - 1):
+        if len(new_triangle[n]) < 2:
+            new_triangle[n + 1][0] += new_triangle[n][0]
+            new_triangle[n + 1][1] += new_triangle[n][0]
+        else:
+            for i in range(0, len(new_triangle[n]) + 1):
+                #print('n: {0}, i: {1}, len: {2}'.format(n, i, len(new_triangle[n])))
+                if i - 1 < 0:
+                    new_triangle[n + 1][i] += new_triangle[n][i]
+                elif i - 1 >= 0 and i < len(new_triangle[n]):
+                    temp = max(new_triangle[n][i - 1], new_triangle[n][i])
+                    new_triangle[n + 1][i] += temp
+                else:
+                    new_triangle[n + 1][i] += new_triangle[n][i - 1]
+                #print(new_triangle)
+
+    result = max(new_triangle[-1])
+    end_time = time.time() - start_time
+    print_time_log(end_time, result)
+    return result
+
+
+def file_reader():
+
+    '''
+    Use readline to read txt file python3:
+    Source: https://stackoverflow.com/questions/28936140/use-readline-to-read-txt-file-python3
+
+    How to get the current working directory using python3:
+    https://stackoverflow.com/questions/17359698/how-to-get-the-current-working-directory-using-python-3
+    :return:
+    '''
+
+    result = []
+    file_path = os.getcwd() + '\p067_triangle.txt'
+    #print(file_path)
+
+    with open(file_path) as source:
+        for line in source:
+            temp = [int(t) for t in line.split(' ')]
+            #print(temp)
+            result.append(temp)
+
+    return result
 
 
 # This function is used for logging processing time only
--- Problem_67/tests/test_problem_67.py
+++ Problem_67/tests/test_problem_67.py
@@ -3,14 +3,56 @@
 
 import unittest
 import os
+from Problem_67.problem_67 import find_maximum_total
+from Problem_67.problem_67 import file_reader
 
 
 class MyTestCase(unittest.TestCase):
-
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
-    def test_sample(self):
-        self.assertEqual(True, False)
+    def test_basic(self):
+        triangle = [[3],
+                    [7, 4],
+                    [2, 4, 6],
+                    [8, 5, 9, 3]]
+
+        expected = sum([3, 7, 4, 9])
+        self.assertEqual(expected, find_maximum_total(triangle))
+
+    def test_basic_2(self):
+        triangle = [[5],
+                    [9, 6],
+                    [4, 6, 8],
+                    [0, 7, 1, 5]]
+
+        expected = sum([5, 9, 6, 7])
+        self.assertEqual(expected, find_maximum_total(triangle))
+
+    def test_big(self):
+        triangle = [[75],
+                    [95, 64],
+                    [17, 47, 82],
+                    [18, 35, 87, 10],
+                    [20, 4, 82, 47, 65],
+                    [19, 1, 23, 75, 3, 34],
+                    [88, 2, 77, 73, 7, 63, 67],
+                    [99, 65, 4, 28, 6, 16, 70, 92],
+                    [41, 41, 26, 56, 83, 40, 80, 70, 33],
+                    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+                    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+                    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+                    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+                    [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+                    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+
+        expected = sum([75, 64, 82, 87, 82, 75, 73, 28, 83, 32, 91, 78, 58, 73, 93])
+        self.assertEqual(expected, find_maximum_total(triangle))
+
+    def test_huge(self):
+        triangle = file_reader()
+
+        expected = 7273
+        self.assertEqual(expected, find_maximum_total(triangle))
 
 
 if __name__ == '__main__':
