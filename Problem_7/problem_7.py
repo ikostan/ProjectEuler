4b24d6a0a73f93632ee024c1927f810c8ccd0e7e
ikostan <igorkostan@gmail.com>
2019-06-20 20:13:53 -0700

    Update problem_7.py
    from Template.problem_ import print_time_log

--- Problem_7/problem_7.py
+++ Problem_7/problem_7.py
@@ -3,6 +3,7 @@
 
 import time
 import math
+from Template.problem_ import print_time_log
 
 
 def prime_generator(limit: int):
@@ -16,7 +17,7 @@ def prime_generator(limit: int):
         n += 1
 
     end_time = time.time() - start_time
-    print_log(end_time, primes[limit - 1])
+    print_time_log(end_time, primes[limit - 1])
     return primes[limit - 1]
 
 
@@ -36,12 +37,3 @@ def is_prime(n: int):
             return False
 
     return True
-
-
-def print_log(end_time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(result, minutes, seconds))
