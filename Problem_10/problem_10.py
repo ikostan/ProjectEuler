1f6c747ab39169e2b7d5e5345375ce88f02e050e
ikostan <igorkostan@gmail.com>
2019-06-20 20:41:20 -0700

    Update problem_10.py

--- Problem_10/problem_10.py
+++ Problem_10/problem_10.py
@@ -3,6 +3,7 @@
 
 import time
 import math
+from Template.problem_ import print_time_log
 
 
 def get_sum(limit: int):
@@ -10,7 +11,7 @@ def get_sum(limit: int):
     primes = primes_generator(limit)
     result = sum(primes)
     end_time = time.time() - start_time
-    print_log(end_time, result)
+    print_time_log(end_time, result)
     return result
 
 
@@ -38,12 +39,3 @@ def is_prime(n: int):
             return False
 
     return True
-
-
-def print_log(end_time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(result, minutes, seconds))
