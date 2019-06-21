38c4b97087a0240a2233858b1afc576076935712
ikostan <igorkostan@gmail.com>
2019-06-20 20:46:32 -0700

    Update problem_13.py

--- Problem_13/problem_13.py
+++ Problem_13/problem_13.py
@@ -2,6 +2,7 @@
 
 
 import time
+from Template.problem_ import print_time_log
 
 
 def get_first_ten_digit(numbers: list, limit: int):
@@ -11,14 +12,3 @@ def get_first_ten_digit(numbers: list, limit: int):
     end_time = time.time() - start_time
     print_time_log(end_time, result)
     return result
-
-
-def print_time_log(end_time: time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
