822692f8432e86b4337371561490d6ccf8529e03
ikostan <igorkostan@gmail.com>
2019-06-20 21:14:05 -0700

    Update problem_67.py

--- Problem_67/problem_67.py
+++ Problem_67/problem_67.py
@@ -4,6 +4,7 @@
 import time
 import os
 import platform
+from utils.utils import print_time_log
 
 
 def find_maximum_total(triangle: list):
@@ -61,16 +62,3 @@ def file_reader():
             result.append(temp)
 
     return result
-
-
-# This function is used for logging processing time only
-# Shows how long it took in order to get the answer
-def print_time_log(end_time: time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
