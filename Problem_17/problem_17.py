e8c05ba58681aed3c1bf94a61c5778f74c90f256
ikostan <igorkostan@gmail.com>
2019-06-20 20:52:21 -0700

    Update problem_17.py

--- Problem_17/problem_17.py
+++ Problem_17/problem_17.py
@@ -2,6 +2,7 @@
 
 
 import time
+from Template.problem_ import print_time_log
 
 
 def number_to_words_counter(numbers: list):
@@ -40,7 +41,6 @@ def number_to_words_counter(numbers: list):
             elif n < 10000:
                 temp = nums_0_19[n // 1000] + nums_dict[1000]
 
-            #print(str(n) + " : " + temp)
             result += temp
         except IndexError:
             print("Error on number: " + str(n))
@@ -49,17 +49,3 @@ def number_to_words_counter(numbers: list):
     end_time = time.time() - start_time
     print_time_log(end_time, result)
     return result
-
-
-# This function is used for logging processing time only
-# Shows how long it took in order to get the answer
-def print_time_log(end_time: time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
-
