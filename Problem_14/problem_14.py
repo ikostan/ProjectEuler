8cfdc5db4676575b9ada04d58b584ae77f2a83a0
ikostan <igorkostan@gmail.com>
2019-06-20 20:46:44 -0700

    Update problem_14.py

--- Problem_14/problem_14.py
+++ Problem_14/problem_14.py
@@ -2,6 +2,7 @@
 
 
 import time
+from Template.problem_ import print_time_log
 
 
 def get_max_counter(starting_number: int):
@@ -59,17 +60,3 @@ def collatz_sequence(starting_number: int, is_first_run=True, counter=1):
 
     counter += 1
     return collatz_sequence(starting_number, False, counter)
-
-
-# This function is used for logging processing time only
-# Shows how long it took in order to get the answer
-def print_time_log(end_time: time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
-
