fda553afc66204d393c39138c73c284986c1f4e2
ikostan <igorkostan@gmail.com>
2019-06-20 21:10:46 -0700

    Update test_problem_.py

--- Template/tests/test_problem_.py
+++ Template/tests/test_problem_.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
 
 
-import unittest.mock
-import os
-import io
-import time
-from Template.problem_ import print_time_log
+import unittest
 
 
 class MyTestCase(unittest.TestCase):
@@ -14,25 +10,6 @@ class MyTestCase(unittest.TestCase):
 
     def test_sample(self):
         self.assertEqual(True, False)
-
-    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_time_log_less_than_minute(self, mock_stdout):
-        start_time = time.time()
-        end_time = time.time() - start_time
-        print_time_log(end_time)
-        captured = mock_stdout.getvalue().replace('\n', '')
-        expected = "The answer {0} returned in {1} seconds".format('', end_time)
-        self.assertEqual(expected, captured)
-
-    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_time_log_more_than_minute(self, mock_stdout):
-        end_time = 65.0
-        answer = 1000
-        print_time_log(end_time, answer)
-        captured = mock_stdout.getvalue().replace('\n', '')
-        expected = "The answer {0} returned in {1} minutes and {2} seconds".format(
-            answer, 1, 5.0)
-        self.assertEqual(expected, captured)
 
 
 if __name__ == '__main__':
