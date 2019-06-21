1ac92200d31fb7b741d9dd4f38ce5af615289fe8
ikostan <igorkostan@gmail.com>
2019-06-20 21:12:39 -0700

    Update problem_18.py

--- Problem_18/problem_18.py
+++ Problem_18/problem_18.py
@@ -2,6 +2,7 @@
 
 
 import time
+from utils.utils import print_time_log
 
 
 def find_maximum_total(triangle: list):
@@ -14,7 +15,7 @@ def find_maximum_total(triangle: list):
             new_triangle[n + 1][1] += new_triangle[n][0]
         else:
             for i in range(0, len(new_triangle[n]) + 1):
-                #print('n: {0}, i: {1}, len: {2}'.format(n, i, len(new_triangle[n])))
+                # print('n: {0}, i: {1}, len: {2}'.format(n, i, len(new_triangle[n])))
                 if i - 1 < 0:
                     new_triangle[n + 1][i] += new_triangle[n][i]
                 elif i - 1 >= 0 and i < len(new_triangle[n]):
@@ -22,23 +23,9 @@ def find_maximum_total(triangle: list):
                     new_triangle[n + 1][i] += temp
                 else:
                     new_triangle[n + 1][i] += new_triangle[n][i - 1]
-                #print(new_triangle)
+                # print(new_triangle)
 
     result = max(new_triangle[-1])
     end_time = time.time() - start_time
     print_time_log(end_time, result)
     return result
-
-
-# This function is used for logging processing time only
-# Shows how long it took in order to get the answer
-def print_time_log(end_time: time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
-
