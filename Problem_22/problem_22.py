a5bf91d76d19820ef7d7d0c465a437aee298253c
ikostan <igorkostan@gmail.com>
2019-06-20 21:13:30 -0700

    Update problem_22.py

--- Problem_22/problem_22.py
+++ Problem_22/problem_22.py
@@ -5,6 +5,7 @@ import time
 import platform
 import os
 import string
+from utils.utils import print_time_log
 
 
 def file_reader():
@@ -71,16 +72,3 @@ def total_of_all_name_scores():
 
     print_time_log(time.time() - start_time, result)
     return result
-
-
-# This function is used for logging processing time only
-# Shows how long it took in order to get the answer
-def print_time_log(end_time: time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
