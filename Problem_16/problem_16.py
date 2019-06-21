542a573f638329a98f12da2805020f8a973cd82f
ikostan <igorkostan@gmail.com>
2019-06-20 20:52:07 -0700

    Update problem_16.py

--- Problem_16/problem_16.py
+++ Problem_16/problem_16.py
@@ -3,6 +3,7 @@
 
 import time
 import math
+from Template.problem_ import print_time_log
 
 
 def find_the_sum(number: int, power: int):
@@ -13,16 +14,3 @@ def find_the_sum(number: int, power: int):
     end_time = time.time() - start_time
     print_time_log(end_time, result)
     return result
-
-
-# This function is used for logging processing time only
-# Shows how long it took in order to get the answer
-def print_time_log(end_time: time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
