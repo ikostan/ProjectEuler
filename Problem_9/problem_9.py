b15b9869af05b4b730b70b60a324cff0e3cc73d6
ikostan <igorkostan@gmail.com>
2019-06-20 20:40:11 -0700

    Update problem_9.py

--- Problem_9/problem_9.py
+++ Problem_9/problem_9.py
@@ -3,12 +3,12 @@
 
 import time
 import math
+from Template.problem_ import print_time_log
 
 
 def find_triplet(num: int):
     start_time = time.time()
     a, b = 1, 1
-    result = 0
 
     while a < (num - 2):
         b = a + 1
@@ -23,19 +23,7 @@ def find_triplet(num: int):
                     print("a:{0}, b:{1}, c:{2}".format(a, b, c))
                     result = a*b*c
                     end_time = time.time() - start_time
-                    print_log(end_time, result)
+                    print_time_log(end_time, result)
                     return result
             b += 1
         a += 1
-
-
-
-
-def print_log(end_time, result):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(result, minutes, seconds))
-
