2bba5bffd300504c9f8f3147fea7f0e0107eb90c
ikostan <igorkostan@gmail.com>
2019-06-20 20:25:34 -0700

    Fixing unit tests

--- Problem_5/problem_5.py
+++ Problem_5/problem_5.py
@@ -2,6 +2,7 @@
 
 
 import time
+from Template.problem_ import print_time_log
 
 
 def smallest_multiple(start: int, end: int):
@@ -30,16 +31,3 @@ def smallest_multiple(start: int, end: int):
     print_time_log(end_time, tested)
 
     return tested
-
-
-# This function is used for logging processing time only
-# Shows how long it took in order to get the answer
-def print_time_log(end_time: time, result=''):
-    if end_time < 60:
-        print("The answer {0} returned in {1} seconds".format(
-            result, end_time))
-    else:
-        minutes = int(end_time / 60)
-        seconds = end_time - (minutes * 60)
-        print("The answer {0} returned in {1} minutes and {2} seconds".format(
-            result, minutes, seconds))
