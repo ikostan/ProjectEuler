526b84a86059d18f2fffd3eb2b05692d26fd4a68
ikostan <igorkostan@gmail.com>
2019-06-21 21:32:29 -0700

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,8 @@
 
 
 import time
+import platform
+import os
 
 
 # This function is used for logging processing time only
@@ -16,3 +18,13 @@ def print_time_log(end_time: time, result=''):
         print("The answer {0} returned in {1} minutes and {2} seconds".format(
             result, minutes, seconds))
 
+
+def get_full_path(file_folder: str, file_name: str):
+    file_path = ''
+    if platform.system() == 'Linux':
+        file_path = os.getcwd() + '/' + file_folder + file_path  # linux
+    elif platform.system() == 'Windows':
+        file_path = os.getcwd() + '\\' + file_name  # windows
+    # elif platform.system() == 'Darwin':
+        # file_path = os.getcwd() + '/Problem_19/tests/leap_years.txt'  # MacOS
+    return file_path
