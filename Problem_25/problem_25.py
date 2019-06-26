557299e0f946767f41fbf272c31675a35407a33a
ikostan <igorkostan@gmail.com>
2019-06-25 19:58:10 -0700

    Problem #25 solved

--- Problem_25/problem_25.py
+++ Problem_25/problem_25.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
 
 
-from utils.utils import print_time_log
 import time
+from utils.utils import print_time_log
 
 
+def find_term(number: list, counter: int, length: int):
+    '''
+    Find the index of the first term in the Fibonacci sequence to contain 'length' digits
+    :param number:
+    :param counter:
+    :param length:
+    :return:
+    '''
+
+    start_time = time.time()
+    while len(str(number[1])) < length:
+        temp = number[1]
+        number[1] += number[0]
+        number[0] = temp
+        counter += 1
+
+    print_time_log(time.time() - start_time, counter)
+    return counter
