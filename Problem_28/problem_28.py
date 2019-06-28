ce4c129aef61bb9043bfb00a27d0ae1cc6eb6da2
ikostan <igorkostan@gmail.com>
2019-06-28 12:25:09 -0700

    Problem 28 -> basic unit tests

--- Problem_28/problem_28.py
+++ Problem_28/problem_28.py
@@ -5,3 +5,39 @@ from utils.utils import print_time_log
 import time
 
 
+def calc_sum(data:list):
+    #center = len(data) // 2 + 1
+    '''
+    Returns sum of the numbers on the diagonals
+    :param data:
+    :return:
+    '''
+    size = len(data)
+    sum = 0
+
+    for a in range(0, size):
+        b = size - 1 - a
+        if b != a:
+            sum += data[a][a] + data[a][b]
+        else:
+            sum += data[a][a]
+
+    return sum
+
+
+def data_generator(size: int):
+    '''
+    Starting with the number 1 and moving to the right in a clockwise direction
+    a 5 by 5 spiral is formed as follows:
+
+        21 22 23 24 25
+        20  7  8  9 10
+        19  6  1  2 11
+        18  5  4  3 12
+        17 16 15 14 13
+
+    :param size:
+    :return:
+    '''
+    data = list()
+    return data
--- Problem_28/tests/test_problem_28.py
+++ Problem_28/tests/test_problem_28.py
@@ -3,14 +3,33 @@
 
 import unittest
 import os
+from Problem_28.problem_28 import calc_sum
+from Problem_28.problem_28 import data_generator
 
 
 class MyTestCase(unittest.TestCase):
 
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
-    def test_sample(self):
-        self.assertEqual(True, False)
+    def test_data_generator_basic(self):
+
+        data = [[21, 22, 23, 24, 25],
+                [20, 7, 8, 9, 10],
+                [19, 6, 1, 2, 11],
+                [18, 5, 4, 3, 12],
+                [17, 16, 15, 14, 13]]
+        size = 5
+        self.assertListEqual(data, data_generator(size))
+
+    def test_calc_sum_basic(self):
+
+        data = [[21, 22, 23, 24, 25],
+                [20, 7, 8, 9, 10],
+                [19, 6, 1, 2, 11],
+                [18, 5, 4, 3, 12],
+                [17, 16, 15, 14, 13]]
+        expected = 101
+        self.assertEqual(expected, calc_sum(data))
 
 
 if __name__ == '__main__':
