80746fc99d118fe537089d66a302b5fec29b29c7
ikostan <igorkostan@gmail.com>
2019-06-29 17:14:15 -0700

    Update problem_2.py
        print_time_log(start_time, result)

--- Problem_2/problem_2.py
+++ Problem_2/problem_2.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 
 import time
+from utils.utils import print_time_log
 
 
 def find_even_fibonacci(max_num):
+
     start_time = time.time()
     nums = generate_fibonacci(max_num)
     result = sum([i for i in nums if i % 2 == 0])
-    print("result {0} returned in {1} seconds".format(result, time.time() - start_time))
+    print_time_log(start_time, result)
     return result
 
 
 def generate_fibonacci(max_num):
+
     nums = [1, 2]
     i = 0
     start = 0
