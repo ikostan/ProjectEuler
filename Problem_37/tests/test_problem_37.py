271d498d5d9b59e8332551f01861f2af24b99bd4
ikostan <igorkostan@gmail.com>
2019-06-30 11:32:12 -0700

    Update test_problem_37.py
    main unit test is ready

--- Problem_37/tests/test_problem_37.py
+++ Problem_37/tests/test_problem_37.py
@@ -3,14 +3,17 @@
 
 import unittest
 import os
+from Problem_37.problem_37 import find_truncatable_primes
 
 
 class MyTestCase(unittest.TestCase):
 
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
-    def test_sample(self):
-        self.assertEqual(True, False)
+    def test_find_truncatable_primes(self):
+        limit = 11
+        expected = 0
+        self.assertEqual(expected, find_truncatable_primes(limit))
 
 
 if __name__ == '__main__':
