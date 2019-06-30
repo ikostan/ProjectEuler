8326e23a1bf25b8046d1de807e5eb5865ffd8846
ikostan <igorkostan@gmail.com>
2019-06-29 17:10:23 -0700

    Update problem_17.py
    Problem_17/problem_17.py                 33      2    94%   45-46

--- Problem_17/problem_17.py
+++ Problem_17/problem_17.py
@@ -23,27 +23,27 @@ def number_to_words_counter(numbers: list):
 
     for n in numbers:
         temp = ''
-        try:
-            if n < 20:
-                temp = nums_0_19[n]
-            elif n < 100:
-                temp = nums_20_90[(n // 10) - 2] + nums_0_19[n % 10]
-            elif n < 1000:
-                temp = nums_0_19[n // 100] + nums_dict[100]
+        # try:
+        if n < 20:
+            temp = nums_0_19[n]
+        elif n < 100:
+            temp = nums_20_90[(n // 10) - 2] + nums_0_19[n % 10]
+        elif n < 1000:
+            temp = nums_0_19[n // 100] + nums_dict[100]
 
-                if n % 100 != 0:
-                    if n % 100 < 20:
-                        temp += 'and' + nums_0_19[n % 100]
-                    elif n % 100 < 100:
-                        temp += 'and' + nums_20_90[((n % 100) // 10) - 2]
-                        if (n % 100) % 10:
-                            temp += nums_0_19[(n % 100) % 10]
-            elif n < 10000:
-                temp = nums_0_19[n // 1000] + nums_dict[1000]
+            if n % 100 != 0:
+                if n % 100 < 20:
+                    temp += 'and' + nums_0_19[n % 100]
+                elif n % 100 < 100:
+                    temp += 'and' + nums_20_90[((n % 100) // 10) - 2]
+                    if (n % 100) % 10:
+                        temp += nums_0_19[(n % 100) % 10]
+        elif n < 10000:
+            temp = nums_0_19[n // 1000] + nums_dict[1000]
 
-            result += temp
-        except IndexError:
-            print("Error on number: " + str(n))
+        result += temp
+        # except IndexError:
+            # print("Error on number: " + str(n))
 
     result = len(result)
     end_time = time.time() - start_time
