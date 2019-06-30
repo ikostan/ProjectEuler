f61117f264305c90c48e798b9bf2c57a50dae495
ikostan <igorkostan@gmail.com>
2019-06-29 18:07:52 -0700

    Update problem_16.py
        print_time_log(start_time, result)

--- Problem_16/problem_16.py
+++ Problem_16/problem_16.py
@@ -7,10 +7,10 @@ from utils.utils import print_time_log
 
 
 def find_the_sum(number: int, power: int):
+
     start_time = time.time()
     powered_number = int(math.pow(number, power))
     n_list = [int(i) for i in str(powered_number) if i != '0']
     result = sum(n_list)
-    end_time = time.time() - start_time
-    print_time_log(end_time, result)
+    print_time_log(start_time, result)
     return result
