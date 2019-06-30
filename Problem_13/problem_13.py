16f7483ea225253d19e28176f534268bbff9f2ac
ikostan <igorkostan@gmail.com>
2019-06-29 18:02:42 -0700

    Update problem_13.py

--- Problem_13/problem_13.py
+++ Problem_13/problem_13.py
@@ -6,9 +6,9 @@ from utils.utils import print_time_log
 
 
 def get_first_ten_digit(numbers: list, limit: int):
+
     start_time = time.time()
     string_number = str(sum(numbers))
     result = int(string_number[:limit])
-    end_time = time.time() - start_time
-    print_time_log(end_time, result)
+    print_time_log(start_time, result)
     return result
