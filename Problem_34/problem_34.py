5664757ff4e7df6248680b6d1ef1b58f2eca7e55
ikostan <igorkostan@gmail.com>
2019-06-29 20:37:33 -0700

    Problem 34 -> solved

--- Problem_34/problem_34.py
+++ Problem_34/problem_34.py
@@ -3,5 +3,49 @@
 
 from utils.utils import print_time_log
 import time
+import math
 
 
+def main(max_number: int):
+
+    start_time = time.time()
+    results = list()
+
+    for n in range(3, max_number + 1):
+        if is_equal(n):
+            results.append(n)
+
+    result = sum(results)
+    print_time_log(start_time, result)
+    return result
+
+
+def sum_of_factorials(number: int):
+    '''
+    Find the sum of all numbers of the factorial of their digits.
+    :param number:
+    :return:
+    '''
+
+    numbers = [int(n) for n in str(number)]
+    results = []
+
+    for number in numbers:
+        result = 1
+        for n in range(1, number + 1):
+            result *= n
+
+        results.append(result)
+
+    return sum(results)
+
+
+def is_equal(number: int):
+    '''
+    Find if the sum of all numbers
+    is equal to the sum of the factorial of their digits.
+    :param number:
+    :return:
+    '''
+
+    return number == sum_of_factorials(number)
--- Problem_34/tests/test_problem_34.py
+++ Problem_34/tests/test_problem_34.py
@@ -3,14 +3,25 @@
 
 import unittest
 import os
+from Problem_34.problem_34 import sum_of_factorials, is_equal, main
 
 
 class MyTestCase(unittest.TestCase):
 
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
-    def test_sample(self):
-        self.assertEqual(True, False)
+    def test_sum_of_factorials_basic(self):
+        number = 145
+        self.assertEqual(number, sum_of_factorials(number))
+
+    def test_sum_is_equal_basic(self):
+        number = 145
+        self.assertEqual(True, is_equal(number))
+
+    def test_main(self):
+        result = 40730
+        max_number = 41000
+        self.assertEqual(result, main(max_number))
 
 
 if __name__ == '__main__':
