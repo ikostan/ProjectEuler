e52a1811556611fe95e286d3cca0d26ebe4643b2
ikostan <igorkostan@gmail.com>
2019-06-30 11:10:18 -0700

    Update problem_36.py
    Some minor comments added

--- Problem_36/problem_36.py
+++ Problem_36/problem_36.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python
 
 
-from utils.utils import print_time_log, is_palindrome
+from utils.utils import print_time_log, is_palindrome, convert_to_binary
 import time
-
-
-def convert_to_binary(number: int):
-    '''
-    code for converting a decimal number to it’s binary equivalent
-    :param number:
-    :return:
-    '''
-    return bin(number).replace("0b","")
 
 
 def calc_sum_of_palindromic_numbers(limit: int):
