6291337f54c6245a5fe7afb062035232f35dcc79
ikostan <igorkostan@gmail.com>
2019-07-07 17:19:03 -0700

    Problem #43 done

--- Problems_1_to_100/Problem_43/problem_43.py
+++ Problems_1_to_100/Problem_43/problem_43.py
@@ -6,7 +6,7 @@ import time
 import itertools
 
 
-def has_property(number: str):
+def has_sub_string_divisibility(number: list):
     '''
     The number, 1406357289, is a 0 to 9 pandigital number.
     Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
@@ -23,38 +23,43 @@ def has_property(number: str):
     :return:
     '''
 
-    number = str(number)
-    size = len(number)
+    if number[0] == 0:
+        return False
 
     # d2d3d4=406 is divisible by 2
-    if int(number[1:4]) % 2 != 0:
-        return False
+    temp = number[1] * 100 + number[2] * 10 + number[3]
+    if temp % 2 == 0 and temp < 1000:
 
-    # d3d4d5=063 is divisible by 3
-    if int(number[2:5]) % 3 != 0:
-        return False
+        # d3d4d5=063 is divisible by 3
+        temp = number[2] * 100 + number[3] * 10 + number[4]
+        if temp % 3 == 0 and temp < 1000:
 
-    # d4d5d6=635 is divisible by 5
-    if int(number[3:6]) % 5 != 0:
-        return False
+            # d4d5d6=635 is divisible by 5
+            temp = number[3] * 100 + number[4] * 10 + number[5]
+            if temp % 5 == 0 and temp < 1000:
 
-    # d5d6d7=357 is divisible by 7
-    if int(number[4:7]) % 7 != 0:
-        return False
+                # d5d6d7=357 is divisible by 7
+                temp = number[4] * 100 + number[5] * 10 + number[6]
+                if temp % 7 == 0 and temp < 1000:
 
-    # d6d7d8=572 is divisible by 11
-    if int(number[5:8]) % 11 != 0:
-        return False
+                    # d6d7d8=572 is divisible by 11
+                    temp = number[5] * 100 + number[6] * 10 + number[7]
+                    if temp % 11 == 0 and temp < 1000:
 
-    # d7d8d9=728 is divisible by 13
-    if int(number[6:9]) % 13 != 0:
-        return False
+                        # d7d8d9=728 is divisible by 13
+                        temp = number[6] * 100 + number[7] * 10 + number[8]
+                        if temp % 13 == 0 and temp < 1000:
 
-    # d8d9d10=289 is divisible by 17
-    if int(number[7:10]) % 17 != 0:
+                            # d8d9d10=289 is divisible by 17
+                            temp = number[7] * 100 + number[8] * 10 + number[9]
+                            if temp % 17 == 0 and temp < 1000:
+                                return True
+                            return False
+                        return False
+                    return False
+                return False
+            return False
         return False
-
-    return True
 
 
 def main():
@@ -63,12 +68,10 @@ def main():
     num_sets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
     combinations = set()
-    for permutation in itertools.permutations(num_sets):
-        str_format = ''.join([str(p) for p in permutation])
-        if str_format[0] != '0' and \
-                is_pandigital(str_format) and \
-                has_property(str_format):
-            combinations.add(int(str_format))
+
+    for combination in itertools.permutations(num_sets):
+        if has_sub_string_divisibility(combination):
+            combinations.add(int(''.join([str(c) for c in combination])))
 
     result = sum(combinations)
     print_time_log(start_time, result)
--- Problems_1_to_100/Problem_43/tests/test_problem_43.py
+++ Problems_1_to_100/Problem_43/tests/test_problem_43.py
@@ -3,7 +3,7 @@
 
 import unittest
 import os
-from Problems_1_to_100.Problem_43.problem_43 import main, has_property
+from Problems_1_to_100.Problem_43.problem_43 import main, has_sub_string_divisibility
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,21 +12,28 @@ class MyTestCase(unittest.TestCase):
 
     def test_has_property_true(self):
 
-        number = 1406357289
-        self.assertTrue(has_property(number))
+        number = [1, 4, 0, 6, 3, 5, 7, 2, 8, 9]
+        self.assertTrue(has_sub_string_divisibility(number))
 
     def test_has_property(self):
 
-        numbers = [1430952867, 1460357289, 1406357289, 4130952867, 4160357289, 4106357289]
+        numbers = [[1, 4, 3, 0, 9, 5, 2, 8, 6, 7],
+                   [1, 4, 6, 0, 3, 5, 7, 2, 8, 9],
+                   [1, 4, 0, 6, 3, 5, 7, 2, 8, 9],
+                   [4, 1, 3, 0, 9, 5, 2, 8, 6, 7],
+                   [4, 1, 6, 0, 3, 5, 7, 2, 8, 9],
+                   [4, 1, 0, 6, 3, 5, 7, 2, 8, 9]]
+
         results = list()
         for number in numbers:
-            results.append(has_property(number))
+            results.append(has_sub_string_divisibility(number))
+
         self.assertTrue(all(results))
 
     def test_has_property_false(self):
 
-        number = 1406357299
-        self.assertFalse(has_property(number))
+        number = [1, 4, 0, 7, 2, 6, 3, 5, 9, 9]
+        self.assertFalse(has_sub_string_divisibility(number))
 
     def test_main(self):
 
