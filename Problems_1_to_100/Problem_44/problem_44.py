3bc2e04b896a2214300ea901121d8466501d46e5
ikostan <igorkostan@gmail.com>
2019-07-07 22:18:44 -0700

    Problem #44 -> DONE
    Pentagon numbers

--- Problems_1_to_100/Problem_44/problem_44.py
+++ Problems_1_to_100/Problem_44/problem_44.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python
 
 
-from utils.utils import print_time_log
+from utils.utils import print_time_log, is_pentagonal, pentagonal_number_generator
 import time
 
 
+def main():
+
+    start_time = time.time()
+    generator = pentagonal_number_generator()
+    pentagonals = set()
+    pentagonals.add(next(generator))
+
+    while True:
+
+        b = next(generator)
+        for a in pentagonals:
+            if is_pentagonal(a + b) and is_pentagonal(abs(a - b)):
+                print_time_log(start_time, abs(a - b))
+                return abs(a - b)
+
+        pentagonals.add(b)
--- Problems_1_to_100/Problem_44/tests/test_problem_44.py
+++ Problems_1_to_100/Problem_44/tests/test_problem_44.py
@@ -3,12 +3,25 @@
 
 import unittest
 import os
+from Problems_1_to_100.Problem_44.problem_44 import main
 
 
 class MyTestCase(unittest.TestCase):
 
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
-    def test_sample(self):
-        pass
+    def test_main(self):
+        '''
+        The first ten pentagonal numbers are:
+        1, 5, 12, 22, 35, 51, 70, 92, 117, 145, ...
+
+        Find the pair of pentagonal numbers, Pj and Pk,
+        for which their sum and difference are pentagonal and D = |Pk − Pj| is minimised;
+
+        what is the value of D?
+        :return:
+        '''
+
+        expected = 5482660
+        self.assertEqual(expected, main())
 
