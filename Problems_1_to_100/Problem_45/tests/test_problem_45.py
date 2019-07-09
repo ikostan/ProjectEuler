6e6d45c93fba8c9c6e215dbf1f9cbcb3a4f1eaa4
ikostan <igorkostan@gmail.com>
2019-07-08 23:21:59 -0700

    Problem 45 skeleton
    Triangular, pentagonal, and hexagonal

---
+++ Problems_1_to_100/Problem_45/tests/test_problem_45.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+import unittest
+import os
+
+
+class MyTestCase(unittest.TestCase):
+
+    print("Running unit tests from: " + os.path.basename(__file__) + "\n")
+
+    def test_sample(self):
+        pass
+
