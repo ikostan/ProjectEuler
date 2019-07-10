29918d7c4df43ca6586f869376f51e66cef1cf1a
ikostan <igorkostan@gmail.com>
2019-07-10 10:34:48 -0700

    problem 52 is correct
    Congratulations, the answer you gave to problem 52 is correct.
    
    You are the 58684th person to have solved this problem.
    
    This problem had a difficulty rating of 5%

--- Problems_1_to_100/Problem_52/problem_52.py
+++ Problems_1_to_100/Problem_52/problem_52.py
@@ -5,3 +5,32 @@ from utils.utils import print_time_log
 import time
 
 
+def is_contain_same_digits(a: int):
+    numbers = []
+    for n in range(2, 7):
+        numbers.append(a * n)
+
+    for n in numbers:
+        if sorted([int(s) for s in str(numbers[0])]) != sorted([int(s) for s in str(n)]):
+            return False
+
+    return True
+
+
+def main(number: int):
+    '''
+    It can be seen that the number, 125874, and its double, 251748,
+    contain exactly the same digits, but in a different order.
+
+    Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+    contain the same digits.
+    :param number:
+    :return:
+    '''
+
+    start_time = time.time()
+    while True:
+        if is_contain_same_digits(number):
+            print_time_log(start_time, number)
+            return number
+        number += 1
--- Problems_1_to_100/Problem_52/tests/test_problem_52.py
+++ Problems_1_to_100/Problem_52/tests/test_problem_52.py
@@ -3,12 +3,14 @@
 
 import unittest
 import os
+from Problems_1_to_100.Problem_52.problem_52 import is_contain_same_digits, main
 
 
 class MyTestCase(unittest.TestCase):
 
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
-    def test_sample(self):
-        pass
-
+    def test_main(self):
+        number = 1
+        expected = 0
+        self.assertEqual(expected, main(number))
