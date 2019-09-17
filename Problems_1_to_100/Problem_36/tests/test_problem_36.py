b6344283f391e928a4b50665763b840e13c2849a
Egor Kostan <igorkostan@gmail.com>
2019-09-16 23:20:31 -0700

    Update test_problem_36.py

--- Problems_1_to_100/Problem_36/tests/test_problem_36.py
+++ Problems_1_to_100/Problem_36/tests/test_problem_36.py
@@ -3,7 +3,8 @@
 
 import unittest
 import os
-from Problems_1_to_100.Problem_36.problem_36 import calc_sum_of_palindromic_numbers
+from Problems_1_to_100.Problem_36.problem_36 \
+    import calc_sum_of_palindromic_numbers
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,7 +14,8 @@ class MyTestCase(unittest.TestCase):
     def test_calc_sum_of_palindromic_numbers(self):
         limit = 1000000
         expected = 872187
-        self.assertEqual(expected, calc_sum_of_palindromic_numbers(limit))
+        self.assertEqual(expected,
+                         calc_sum_of_palindromic_numbers(limit))
 
 
 if __name__ == '__main__':
