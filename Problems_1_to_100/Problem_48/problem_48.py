df4a8675ef6b9c1dcc1d4c85e5401bd7385d9829
Egor Kostan <igorkostan@gmail.com>
2019-09-16 20:44:24 -0700

    Update problem_48.py

--- Problems_1_to_100/Problem_48/problem_48.py
+++ Problems_1_to_100/Problem_48/problem_48.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-
 from utils.utils import print_time_log
 import time
 
 
 def find_last_ten_digits(start: int, end: int):
     '''
-    Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+    Find the last ten digits of the series,
+    11 + 22 + 33 + ... + 10001000.
     :param start:
     :param end:
     :return:
@@ -22,4 +22,3 @@ def find_last_ten_digits(start: int, end: int):
     result = total[- 10:]
     print_time_log(start_time, result)
     return result
-
