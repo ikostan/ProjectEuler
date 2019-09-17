1baffb12faa95add1a5d0ae2e0db0adfa705bb01
Egor Kostan <igorkostan@gmail.com>
2019-09-16 23:20:35 -0700

    Update problem_35.py

--- Problems_1_to_100/Problem_35/problem_35.py
+++ Problems_1_to_100/Problem_35/problem_35.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
-
-from utils.utils import print_time_log, is_prime
 import time
+from utils.utils import print_time_log, is_prime
 
 
 def get_rotations(digit: int):
-    '''
-    The number, 197 has following rotations of the digits: 197, 971, and 719
+    """
+    The number, 197 has following rotations of the digits:
+    197, 971, and 719
 
     :param digit:
     :return rotations:
-    '''
+    """
 
     rotations = set()
     rotations.add(digit)
@@ -31,7 +31,7 @@ def get_rotations(digit: int):
 
 
 def is_circular(digit: int, debug=False):
-    '''
+    """
     The number, 197, is called a circular prime
     because all rotations of the digits: 197, 971, and 719,
     are themselves prime.
@@ -39,7 +39,7 @@ def is_circular(digit: int, debug=False):
     :param digit:
     :param debug:
     :return:
-    '''
+    """
 
     results = list()
     combinations = get_rotations(digit)
@@ -54,7 +54,7 @@ def is_circular(digit: int, debug=False):
 
 
 def is_circular_pattern(digit: int):
-    '''
+    """
     Circular primes:
 
     2, 3, 5, 7, 13, 17, 37, 79,
@@ -63,7 +63,7 @@ def is_circular_pattern(digit: int):
 
     :param digit:
     :return:
-    '''
+    """
 
     pattern = '1379'
 
@@ -79,15 +79,15 @@ def main(max_limit: int):
 
     start_time = time.time()
     circulars = set()
-    primes = [n for n in range(1, max_limit) if is_prime(n) and is_circular_pattern(n)]
-    # print('Finished with calculating prime numbers: {}'.format(time.time() - start_time))
+    primes = [n for n in range(1, max_limit)
+              if is_prime(n)
+              and is_circular_pattern(n)]
 
     for n in primes:
         if n not in circulars and is_circular(n):
             for p in get_rotations(n):
                 circulars.add(p)
 
-    # print(sorted(circulars))
     result = len(circulars)
     print_time_log(start_time, result)
     return result
