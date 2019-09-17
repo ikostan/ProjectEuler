350416a1dbff063bf2a76d9fe224e8f1801ce348
Egor Kostan <igorkostan@gmail.com>
2019-09-16 17:19:33 -0700

    Update problem_1.py

--- Problems_1_to_100/Problem_1/problem_1.py
+++ Problems_1_to_100/Problem_1/problem_1.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+"""
+Problem 1: Multiples of 3 and 5
+Source: https://github.com/ikostan/ProjectEuler/tree/master/Problem_1
+"""
 
 
 import time
@@ -6,6 +10,12 @@ from utils.utils import print_time_log
 
 
 def get_multiplies_of_3_and_5(max_num, nums):
+    """
+    Returns all multiplies of 3 and 5 within specified range
+    :param max_num:
+    :param nums:
+    :return:
+    """
     start_time = time.time()
     result = 0
     i = 0
@@ -20,8 +30,13 @@ def get_multiplies_of_3_and_5(max_num, nums):
 
 
 def div_by_nums(i, nums):
+    """
+    Test if number (i) is divisible by array of numbers (nums)
+    :param i:
+    :param nums:
+    :return:
+    """
     for n in nums:
         if i % n == 0:
             return True
     return False
-
