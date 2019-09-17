73a68cab5a398820280d45175e7d541d577f7e3c
Egor Kostan <igorkostan@gmail.com>
2019-09-16 23:06:45 -0700

    Update problem_25.py
    E501 line too long (89 > 79 characters)

--- Problems_1_to_100/Problem_25/problem_25.py
+++ Problems_1_to_100/Problem_25/problem_25.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 
-
 import time
 from utils.utils import print_time_log
 
 
 def find_term(number: list, counter: int, length: int):
-    '''
-    Find the index of the first term in the Fibonacci sequence to contain 'length' digits
+    """
+    Find the index of the first term in the Fibonacci
+    sequence to contain 'length' digits
+    
     :param number:
     :param counter:
     :param length:
     :return:
-    '''
+    """
 
     start_time = time.time()
     while len(str(number[1])) < length:
