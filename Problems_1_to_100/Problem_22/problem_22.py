1248550e5771119a457fb406560e99e70a49b695
Egor Kostan <igorkostan@gmail.com>
2019-09-16 23:03:43 -0700

    Update problem_22.py
    E501 line too long (83 > 79 characters)

--- Problems_1_to_100/Problem_22/problem_22.py
+++ Problems_1_to_100/Problem_22/problem_22.py
@@ -51,7 +51,9 @@ def total_of_all_name_scores():
     Calculates the total of all the name scores in the file
 
     For example, when the list is sorted into alphabetical order,
-    COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+    COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53,
+    is the 938th name in the list.
+
     So, COLIN would obtain a score of 938 × 53 = 49714.
 
     :return:
