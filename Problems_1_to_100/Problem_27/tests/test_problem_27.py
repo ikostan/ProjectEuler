4be78fc7b067fd542613269e7af3482cb83e700a
Egor Kostan <igorkostan@gmail.com>
2019-09-16 23:09:56 -0700

    Update test_problem_27.py

--- Problems_1_to_100/Problem_27/tests/test_problem_27.py
+++ Problems_1_to_100/Problem_27/tests/test_problem_27.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import unittest
 import os
 from Problems_1_to_100.Problem_27.problem_27 import main
@@ -11,7 +10,7 @@ class MyTestCase(unittest.TestCase):
     print("Running unit tests from: " + os.path.basename(__file__) + "\n")
 
     def test_sample(self):
-        #expected = 0
-        #self.assertEqual(expected, main()['product'])
+        # expected = 0
+        # self.assertEqual(expected, main()['product'])
         pass
 
