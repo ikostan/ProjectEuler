542d42315f8ae07cf39431c5a49e739ebadfa8b4
Egor Kostan <igorkostan@gmail.com>
2019-09-16 19:46:22 -0700

    Update problem_15.py

--- Problems_1_to_100/Problem_15/problem_15.py
+++ Problems_1_to_100/Problem_15/problem_15.py
@@ -8,7 +8,8 @@ from utils.utils import print_time_log
 def calc_lattice_path(width: int, height: int):
 
     start_time = time.time()
-    head = multiply_members(list(range(max(width, height) + 1, width + height + 1)))
+    head = multiply_members(list(range(max(width, height) + 1,
+                                       width + height + 1)))
 
     if width == height:
         tail = multiply_members(list(range(1, max(width, height) + 1)))
