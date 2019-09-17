2fe85b875e92d66becddd687b576fe553e1ce36c
Egor Kostan <igorkostan@gmail.com>
2019-09-16 19:54:13 -0700

    Update problem_21.py

--- Problems_1_to_100/Problem_21/problem_21.py
+++ Problems_1_to_100/Problem_21/problem_21.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 
-
 import time
 from utils.utils import print_time_log
 
 
 def sum_of_proper_divisors(number: int):
-    '''
+    """
     Let d(n) be defined as the sum of proper divisors of n
     (numbers less than n which divide evenly into n).
     :param number:
     :return:
-    '''
+    """
     divisors = []
 
     for n in range(1, number):
@@ -22,11 +21,11 @@ def sum_of_proper_divisors(number: int):
 
 
 def sum_of_amicable_numbers(number: int):
-    '''
+    """
     Evaluate the sum of all the amicable numbers under -> number
     :param number:
     :return:
-    '''
+    """
     start_time = time.time()
     amicable = set()
 
@@ -35,11 +34,9 @@ def sum_of_amicable_numbers(number: int):
             a = sum_of_proper_divisors(n)
             b = sum_of_proper_divisors(a)
             if (n == b) and not (n == b == a):
-                # print('n: {0}, a: {1}, b: {2}'.format(n, a, b)) #  debug only
                 amicable.add(n)
                 amicable.add(a)
 
     result = sum(amicable)
-    # print('amicable: {0}, result: {1}'.format(amicable, result)) #  debug only
     print_time_log(start_time, result)
     return result
