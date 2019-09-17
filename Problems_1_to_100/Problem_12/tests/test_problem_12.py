e0320ee108a7a93469ded2918c4d1c2defb83708
Egor Kostan <igorkostan@gmail.com>
2019-09-16 19:44:37 -0700

    Update test_problem_12.py

--- Problems_1_to_100/Problem_12/tests/test_problem_12.py
+++ Problems_1_to_100/Problem_12/tests/test_problem_12.py
@@ -13,17 +13,20 @@ class MyTestCase(unittest.TestCase):
     def test_triangle_number_generator(self):
         dividers = 5
         expected = 28
-        self.assertEqual(expected, triangle_number_generator(dividers)['triangle'])
+        self.assertEqual(expected,
+                         triangle_number_generator(dividers)['triangle'])
 
     def test_50(self):
         dividers = 50
         expected = 25200
-        self.assertEqual(expected, triangle_number_generator(dividers)['triangle'])
+        self.assertEqual(expected,
+                         triangle_number_generator(dividers)['triangle'])
 
     def test_100(self):
         dividers = 100
         expected = 73920
-        self.assertEqual(expected, triangle_number_generator(dividers)['triangle'])
+        self.assertEqual(expected,
+                         triangle_number_generator(dividers)['triangle'])
 
     def test_150(self):
         dividers = 150
@@ -33,13 +36,11 @@ class MyTestCase(unittest.TestCase):
     def test_250(self):
         dividers = 250
         expected = 2162160
-        self.assertEqual(expected, triangle_number_generator(dividers)['triangle'])
+        self.assertEqual(expected,
+                         triangle_number_generator(dividers)['triangle'])
 
     def test_500(self):
         dividers = 500
         expected = 76576500
-        self.assertEqual(expected, triangle_number_generator(dividers)['triangle'])
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(expected,
+                         triangle_number_generator(dividers)['triangle'])
