59f513b52928c28526487670486568a4bc8f3756
Egor Kostan <igorkostan@gmail.com>
2019-09-16 23:06:14 -0700

    Update problem_24.py
    E501 line too long (102 > 79 characters)

--- Problems_1_to_100/Problem_24/problem_24.py
+++ Problems_1_to_100/Problem_24/problem_24.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 
-
-from utils.utils import print_time_log
 import time
 import itertools
+from utils.utils import print_time_log
 
 
 def lexicographic_permutations(numbers: list, index: int):
-    '''
+    """
     What is the millionth lexicographic permutation of
     the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
     :param numbers:
     :param index:
     :return:
-    '''
+    """
+
     start_time = time.time()
-    result = int(''.join([str(l) for l in [list(i) for i in itertools.permutations(numbers)][index]]))
+    result = int(''.join([str(l)
+                          for l in [list(i)
+                                    for i in itertools.permutations(numbers)][index]]))
     print_time_log(start_time, result)
     return result
