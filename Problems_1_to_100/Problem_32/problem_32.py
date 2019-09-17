387f7d5d9d5fecd45b6567e4774313bbc14615a4
Egor Kostan <igorkostan@gmail.com>
2019-09-16 23:16:58 -0700

    Update problem_32.py

--- Problems_1_to_100/Problem_32/problem_32.py
+++ Problems_1_to_100/Problem_32/problem_32.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python
 
-
-from utils.utils import print_time_log
 import time
+from utils.utils import print_time_log
 
 
 def is_pandigital(multiplicand: int, multiplier: int, pattern: str):
-    '''
+    """
     We shall say that an n-digit number is pandigital
     if it makes use of all the digits 1 to n exactly once;
     for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
     :param multiplicand:
     :param multiplier:
     :param pattern:
     :return:
-    '''
+    """
 
     product = multiplicand * multiplier
     numbers = [multiplicand, multiplier, product]
-    result = ''.join(sorted([char for char in ''.join([str(n) for n in numbers])], reverse=True))
+    result = ''.join(sorted([char
+                             for char in ''.join([str(n)
+                                                  for n in numbers])], reverse=True))
 
     return result == pattern
 
 
 def sum_of_all_products(limit: int):
-    '''
+    """
     Find the sum of all products whose multiplicand/multiplier/product
     identity can be written as a 1 through 9 pandigital.
+
     :param limit:
     :return:
-    '''
+    """
 
     start_time = time.time()
     pattern = str(limit)
