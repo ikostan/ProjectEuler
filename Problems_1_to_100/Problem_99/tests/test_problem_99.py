c8f61bbc36896b56547592d6e066b50c532d87f1
Egor Kostan <igorkostan@gmail.com>
2019-09-17 19:06:21 -0700

    Update test_problem_99.py
    E501 line too long (92 > 79 characters)

--- Problems_1_to_100/Problem_99/tests/test_problem_99.py
+++ Problems_1_to_100/Problem_99/tests/test_problem_99.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 
-
 import unittest
 import os
-from Problems_1_to_100.Problem_99.problem_99 import get_largest_result_line_num, file_reader
+from Problems_1_to_100.Problem_99.problem_99 \
+    import get_largest_result_line_num, file_reader
 
 
 class MyTestCase(unittest.TestCase):
 
-    print("Running unit tests from: " + os.path.basename(__file__) + "\n")
+    print("Running unit tests from: " +
+          os.path.basename(__file__) + "\n")
 
     def test_get_largest_result_line_num(self):
         # The line with the largest number is 709
         expected = 709
-        self.assertEqual(expected, get_largest_result_line_num(file_reader()))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(expected,
+                         get_largest_result_line_num(file_reader()))
