335af1568abbb6ef9d7399c331c6f4f1dfd54851
Egor Kostan <igorkostan@gmail.com>
2019-09-17 19:04:16 -0700

    Update test_problem_7.py
    E501 line too long (80 > 79 characters)

--- Problems_1_to_100/Problem_7/tests/test_problem_7.py
+++ Problems_1_to_100/Problem_7/tests/test_problem_7.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python
 
-
 import unittest
 import os
-from Problems_1_to_100.Problem_7.problem_7 import prime_generator
+from Problems_1_to_100.Problem_7.problem_7 \
+    import prime_generator
 
 
 class MyTestCase(unittest.TestCase):
 
-    print("Running unit tests from: " + os.path.basename(__file__) + "\n")
+    print("Running unit tests from: " +
+          os.path.basename(__file__) + "\n")
 
     def test_basic(self):
         limit = 6
         expected = 13
-        self.assertEqual(expected, prime_generator(limit))
+        self.assertEqual(expected,
+                         prime_generator(limit))
 
     def test_10001(self):
         limit = 10001
         expected = 104743
-        self.assertEqual(expected, prime_generator(limit))
+        self.assertEqual(expected,
+                         prime_generator(limit))
 
-    @unittest.skip("the test is too long and has no real value for the problem")
+    @unittest.skip("the test is too long and has "
+                   "no real value for the problem")
     def test_huge(self):
         limit = 100000
         expected = 0
-        self.assertEqual(expected, prime_generator(limit))
+        self.assertEqual(expected,
+                         prime_generator(limit))
