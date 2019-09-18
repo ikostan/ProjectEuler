b30724c46e3a94687e55f2a366df285885fdbe6b
Egor Kostan <igorkostan@gmail.com>
2019-09-17 18:21:39 -0700

    Update problem_41.py
    E501 line too long (81 > 79 characters)

--- Problems_1_to_100/Problem_41/problem_41.py
+++ Problems_1_to_100/Problem_41/problem_41.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 
-
-from utils.utils import print_time_log, is_pandigital, is_prime
 import time
 import itertools
+from utils.utils import print_time_log, is_pandigital, is_prime
 
 
 def find_largest_pandigital_prime(numbers: list):
-    '''
-    We shall say that an n-digit number is pandigital if it makes use of all
+    """
+    We shall say that an n-digit number is
+    pandigital if it makes use of all
     the digits 1 to n exactly once.
+
     For example, 2143 is a 4-digit pandigital and is also prime.
     What is the largest n-digit pandigital prime that exists?
+
+    :param numbers:
     :param number:
     :return:
-    '''
+    """
 
     start_time = time.time()
     permutations = list(
-        int(''.join(t)) for t in itertools.permutations(str(t) for t in numbers))
+        int(''.join(t))
+        for t in itertools.permutations(str(t)
+                                        for t in numbers))
 
     big_pandigital_primes = set()
     for n in permutations:
