27ee89a1cb1e473edb0af119fd0c87394c75652d
Egor Kostan <igorkostan@gmail.com>
2019-09-17 18:29:47 -0700

    Update test_problem_45.py
    E501 line too long (89 > 79 characters)

--- Problems_1_to_100/Problem_45/tests/test_problem_45.py
+++ Problems_1_to_100/Problem_45/tests/test_problem_45.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 
-
 import unittest
 import os
-from Problems_1_to_100.Problem_45.problem_45 import main
+from Problems_1_to_100.Problem_45.problem_45 \
+    import main
 
 
 class MyTestCase(unittest.TestCase):
 
-    print("Running unit tests from: " + os.path.basename(__file__) + "\n")
+    print("Running unit tests from: " +
+          os.path.basename(__file__) + "\n")
 
     def test_main(self):
-        '''
+        """
         It can be verified that T285 = P165 = H143 = 40755.
 
-        Every hexagonal number is a triangular number since: r(2r-1)=1/2(2r-1)[(2r-1)+1].
+        Every hexagonal number is a triangular number since:
+        r(2r-1)=1/2(2r-1)[(2r-1)+1].
         Source: http://mathworld.wolfram.com/HexagonalNumber.html
 
         Every pentagonal number is 1/3 of a triangular number.
@@ -22,7 +24,7 @@ class MyTestCase(unittest.TestCase):
 
         Therefore, index = 166
         :return:
-        '''
+        """
 
         index = 166
         expected = 1533776805
