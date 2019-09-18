11f89a4442742a6c5f077d3c3691ed6a6f98863e
Egor Kostan <igorkostan@gmail.com>
2019-09-17 18:30:26 -0700

    Update problem_6.py
    E501 line too long (81 > 79 characters)

--- Problems_1_to_100/Problem_6/problem_6.py
+++ Problems_1_to_100/Problem_6/problem_6.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import time
 import math
 from utils.utils import print_time_log
@@ -9,7 +8,8 @@ from utils.utils import print_time_log
 def square_diff(start, end):
 
     start_time = time.time()
-    result = int(square_of_sums(start, end) - sum_squares_of_numbers(start, end))
+    result = int(square_of_sums(start, end) -
+                 sum_squares_of_numbers(start, end))
     print_time_log(start_time, result)
     return result
 
