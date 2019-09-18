4e1693e1c5fd6b4a6cbc7dc52d58fb5c0cf3ed3a
Egor Kostan <igorkostan@gmail.com>
2019-09-17 19:07:46 -0700

    Update test_utils.py
    E501 line too long (95 > 79 characters)

--- utils/tests/test_utils.py
+++ utils/tests/test_utils.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import unittest.mock
 import platform
 import os
@@ -16,7 +15,8 @@ from utils.utils import print_time_log, get_full_path, is_prime, \
 
 class MyTestCase(unittest.TestCase):
 
-    print("Running unit tests from: " + os.path.basename(__file__) + "\n")
+    print("Running unit tests from: " +
+          os.path.basename(__file__) + "\n")
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_print_time_log_less_than_minute(self, mock_stdout):
@@ -26,11 +26,14 @@ class MyTestCase(unittest.TestCase):
         end_time = time.time() - start_time
         print_time_log(start_time)
         captured = mock_stdout.getvalue().replace('\n', '')
-        expected = "The answer {0} returned in {1} seconds".format('', int(round(end_time, 3)))
+        expected = "The answer {0} " \
+                   "returned in {1} seconds".format('',
+                                                    int(round(end_time, 3)))
         self.assertEqual(expected, captured)
 
     @unittest.skip("Fails on python 3.5")
-    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    @unittest.mock.patch('sys.stdout',
+                         new_callable=io.StringIO)
     def test_print_time_log_more_than_minute(self, mock_stdout):
 
         answer = 1000
@@ -39,7 +42,10 @@ class MyTestCase(unittest.TestCase):
         end_time = time.time() - start_time
         print_time_log(start_time, answer)
         captured = mock_stdout.getvalue().replace('\n', '')
-        expected = "The answer {0} returned in {1} minutes and {2} seconds".format(
+        expected = "The answer {0} " \
+                   "returned in {1} " \
+                   "minutes and {2} " \
+                   "seconds".format(
             answer, int(end_time // 60), round(end_time % 60, 3))
         self.assertEqual(expected, captured)
 
@@ -65,7 +71,8 @@ class MyTestCase(unittest.TestCase):
             expected = ['/home/travis/build/ikostan/ProjectEuler/utils/tests/test.txt',
                         '/home/circleci/project/utils/tests/test.txt']
 
-        self.assertTrue(get_full_path(folder_name, file_name) in expected)
+        self.assertTrue(get_full_path(folder_name,
+                                      file_name) in expected)
 
     def test_get_full_path_linux(self):
         target_os = 'Linux'
@@ -77,7 +84,8 @@ class MyTestCase(unittest.TestCase):
         if platform.system() == 'Windows':
             expected = os.getcwd() + '/utils/tests' + '/test.txt'
 
-        self.assertTrue(get_full_path(folder_name, file_name, target_os) in expected)
+        self.assertTrue(get_full_path(folder_name, file_name,
+                                      target_os) in expected)
 
     def test_is_prime_2(self):
         self.assertEqual(True, is_prime(2))
